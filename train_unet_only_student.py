ef64179b0290a7fe4f919a374bf472d4d36efc6c
junseo0421 <jmdmk9st@naver.com>
2024-11-25 17:35:30 +0900

    total로 바꿈

--- train_unet_only_student.py
+++ train_unet_only_student.py
@@ -102,7 +102,7 @@ def train(gen, dis, opt_gen, opt_dis, epoch, train_loader, writer):  #24.09.19 r
             ## feature size 맞춰주기 f_de 와 f_en
             ## Generate Image
             # I_pred, f_de = gen(mask_img)  # 생성된 image(2, 3, 192, 192), 중간 feature map
-            gen_step_start_time = time.time()
+            total_step_start_time = time.time()
             I_pred = gen(mask_img)
             # f_en = gen(iner_img, only_encode=True)  #iner_img(GT)를 encoding하여 feature map을 얻음
 
@@ -161,7 +161,7 @@ def train(gen, dis, opt_gen, opt_dis, epoch, train_loader, writer):  #24.09.19 r
             opt_gen.zero_grad()
             gen_loss.backward()
             opt_gen.step()
-            gen_step_time = time.time() - gen_step_start_time
+            total_step_time = time.time() - total_step_start_time
 
             acc_pixel_rec_loss += pixel_rec_loss.data
             acc_gen_adv_loss += gen_adv_loss.data
@@ -176,7 +176,7 @@ def train(gen, dis, opt_gen, opt_dis, epoch, train_loader, writer):  #24.09.19 r
 
             # tqdm의 상태 업데이트
             pbar.update(1)
-            pbar.set_postfix({'gen_loss': gen_loss.item(), 'dis_loss': dis_loss.item(), 'dis_time': f"{dis_step_time:.2f}s", 'gen_time': f"{gen_step_time:.2f}s",})
+            pbar.set_postfix({'gen_loss': gen_loss.item(), 'dis_loss': dis_loss.item(), 'dis_time': f"{dis_step_time:.2f}s", 'total_time': f"{total_step_time:.2f}s",})
 
     ## Tensor board
     writer.add_scalars('train/generator_loss',
