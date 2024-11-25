3d07393babcc801444e0c0c1416e74dd41b133b0
junseo0421 <jmdmk9st@naver.com>
2024-11-25 15:15:29 +0900

    24.11.25 D_Thin_Sep_UNet_4 추가

--- models/unet/sep_unet_model.py
+++ models/unet/sep_unet_model.py
@@ -109,16 +109,65 @@ class Sep_UNet_4(nn.Module):  # m = 4, feature 1/2
         self.down2 = torch.utils.checkpoint(self.down2)
         self.down3 = torch.utils.checkpoint(self.down3)
         self.down4 = torch.utils.checkpoint(self.down4)
-        self.down5 = torch.utils.checkpoint(self.down5)
-        self.down6 = torch.utils.checkpoint(self.down6)
         self.up1 = torch.utils.checkpoint(self.up1)
         self.up2 = torch.utils.checkpoint(self.up2)
         self.up3 = torch.utils.checkpoint(self.up3)
         self.up4 = torch.utils.checkpoint(self.up4)
-        self.up5 = torch.utils.checkpoint(self.up5)
-        self.up6 = torch.utils.checkpoint(self.up6)
         self.outc = torch.utils.checkpoint(self.outc)
 
+class D_Thin_Sep_UNet_4(nn.Module):  # m = 4, feature 1/4
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(D_Thin_Sep_UNet_4, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        # Encoder
+        self.inc = (DoubleConv(n_channels, 16))  # first two separable conv -> regular conv
+        self.down1 = (SepDown(16, 32))
+        self.down2 = (SepDown(32, 64))
+        self.down3 = (SepDown(64, 128))
+        factor = 2 if bilinear else 1
+        self.down4 = (SepDown(128, 256 // factor))  # bottleneck
+
+        # decoder
+        self.up1 = (SepUp(256, 128 // factor, bilinear))
+        self.up2 = (SepUp(128, 64 // factor, bilinear))
+        self.up3 = (SepUp(64, 32 // factor, bilinear))
+        self.up4 = (SepUp(32, 16, bilinear))
+
+        # Output layer
+        self.outc = (OutConv(16, n_classes))
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # decoder
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+
+        #output
+        logits = self.outc(x)
+        return logits
+
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.down3 = torch.utils.checkpoint(self.down3)
+        self.down4 = torch.utils.checkpoint(self.down4)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.up3 = torch.utils.checkpoint(self.up3)
+        self.up4 = torch.utils.checkpoint(self.up4)
+        self.outc = torch.utils.checkpoint(self.outc)
 
 class Sep_UNet_6(nn.Module):  # m = 6, feature 1/2
     def __init__(self, n_channels, n_classes, bilinear=False):
