0d5c9ac9624a226b28df48395ebb92f5fc9723ab
junseo0421 <jmdmk9st@naver.com>
2024-12-22 23:16:19 +0900

    24.12.22 sddb2

--- train_transformer4_proposed_v2_afa_KD.py
+++ train_transformer4_proposed_v2_afa_KD.py
@@ -305,7 +305,7 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer, teacher_gen):
                        epoch)
 
 if __name__ == '__main__':
-    NAME_DATASET = 'HKdb-1'
+    NAME_DATASET = 'SDdb-2'
     SAVE_BASE_DIR = '/content/drive/MyDrive/afa_kd_out_only/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET, 'checkpoints')  # 24.09.25 HKdb-2
