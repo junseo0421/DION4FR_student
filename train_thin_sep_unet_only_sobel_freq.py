07f5680936f7334f2ef454a3e6beb9846f36e210
junseo0421 <jmdmk9st@naver.com>
2025-01-06 17:00:03 +0900

    hkdb2로 숮어정ㅅ정

--- train_thin_sep_unet_only_sobel_freq.py
+++ train_thin_sep_unet_only_sobel_freq.py
@@ -263,7 +263,7 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer):
 
 if __name__ == '__main__':
     NAME_DATASET = 'HKdb-2'
-    SAVE_BASE_DIR = '/content/drive/MyDrive/dq_u_net_sep4_sobel_freq_all_modified/output'
+    SAVE_BASE_DIR = '/content/drive/MyDrive/dq_u_net_sep4_sobel_freq_all/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET , 'checkpoints')
     SAVE_LOG_DIR = join(SAVE_BASE_DIR, NAME_DATASET , 'logs_all')
