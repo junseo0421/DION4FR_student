1fa2322ef043a4608080dcf682f770d9c82c10e0
junseo0421 <jmdmk9st@naver.com>
2025-01-21 12:02:24 +0900

    25.01.21 hkdb1 dunet afa only

--- train_thin_sep_unet_only_afa.py
+++ train_thin_sep_unet_only_afa.py
@@ -262,7 +262,7 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer):
                        epoch)
 
 if __name__ == '__main__':
-    NAME_DATASET = 'HKdb-2'
+    NAME_DATASET = 'HKdb-1'
     SAVE_BASE_DIR = '/content/drive/MyDrive/d_u_net_sep4_afa/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET , 'checkpoints')
