f1f89a176c5932f9c443689e14f2620408978c83
junseo0421 <jmdmk9st@naver.com>
2025-01-22 13:20:51 +0900

    25.01.22 hkdb2 unetsep4 500-600

--- train_unet_only_student.py
+++ train_unet_only_student.py
@@ -235,8 +235,8 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer):
                        epoch)
 
 if __name__ == '__main__':
-    NAME_DATASET = 'SDdb-2'
-    SAVE_BASE_DIR = '/content/drive/MyDrive/dq_u_net_sep4/output'
+    NAME_DATASET = 'HKdb-2'
+    SAVE_BASE_DIR = '/content/drive/MyDrive/u_net_sep4/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET, 'checkpoints')
     SAVE_LOG_DIR = join(SAVE_BASE_DIR, NAME_DATASET, 'logs_all')
@@ -251,12 +251,12 @@ if __name__ == '__main__':
 
         parser = argparse.ArgumentParser()
 
-        parser.add_argument('--train_batch_size', type=int, help='batch size of training data', default=2)
+        parser.add_argument('--train_batch_size', type=int, help='batch size of training data', default=8)
         parser.add_argument('--test_batch_size', type=int, help='batch size of testing data', default=16)
-        parser.add_argument('--epochs', type=int, help='number of epoches', default=500)
+        parser.add_argument('--epochs', type=int, help='number of epoches', default=600)
         parser.add_argument('--lr', type=float, help='learning rate', default=0.0004)
         parser.add_argument('--alpha', type=float, help='learning rate decay for discriminator', default=0.1)
-        parser.add_argument('--load_pretrain', type=bool, help='load pretrain weight', default=False)  # pretrain !!
+        parser.add_argument('--load_pretrain', type=bool, help='load pretrain weight', default=True)  # pretrain !!
         parser.add_argument('--test_flag', type=bool, help='testing while training', default=False)
         parser.add_argument('--adjoint', type=bool, help='if use adjoint in odenet', default=True)
 
@@ -364,7 +364,8 @@ if __name__ == '__main__':
     print('Initializing model...')
     # gen = UNet(n_channels=3, n_classes=3).cuda()  # student model
     # gen = Q_Thin_Sep_UNet_4(n_channels=3, n_classes=3).cuda()
-    gen = DQ_Thin_Sep_UNet_4(n_channels=3, n_classes=3).cuda()
+    # gen = DQ_Thin_Sep_UNet_4(n_channels=3, n_classes=3).cuda()
+    gen = Sep_UNet_4(n_channels=3, n_classes=3).cuda()
 
     # 24.10.11 모델 파라미터 수 출력
     print_model_parameters(gen)
@@ -378,7 +379,7 @@ if __name__ == '__main__':
 
     # Load pre-trained weight
     if args.load_pretrain:
-        start_epoch = 490
+        start_epoch = 500
         print(f'Loading model weight...at epoch {start_epoch}')
         gen.load_state_dict(torch.load(join(args.load_weight_dir, f'Gen_former_{start_epoch}.pt')))
         dis.load_state_dict(torch.load(join(args.load_weight_dir, f'Dis_former_{start_epoch}.pt')))
