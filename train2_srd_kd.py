060955210e495933144a2509b345dbef221e52ff
junseo0421 <jmdmk9st@naver.com>
2025-02-09 13:30:39 +0900

    25.02.09 srd kd 2 sddb1

--- train2_srd_kd.py
+++ train2_srd_kd.py
@@ -305,7 +305,7 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer, teacher_gen):
 
 if __name__ == '__main__':
     # model forward 수정해야함!!!
-    NAME_DATASET = 'HKdb-1'
+    NAME_DATASET = 'SDdb-1'
     SAVE_BASE_DIR = '/content/drive/MyDrive/srd_kd/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET, 'checkpoints')
