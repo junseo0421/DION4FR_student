da817f4378e6669aa10f59070ee380d472719587
junseo0421 <jmdmk9st@naver.com>
2025-02-27 18:57:24 +0900

    25.02.27 rrd kd 2 hkdb1

--- loss.py
+++ loss.py
@@ -4,8 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 import numpy as np
+import math
 
-# from pytorch_wavelets import DWTForward, DWTInverse
+from pytorch_wavelets import DWTForward, DWTInverse
 
 class SSIM_loss(nn.Module):
     """Layer to compute the SSIM loss between a pair of images
@@ -451,11 +452,11 @@ class SP(nn.Module):
 
     def forward(self, fm_s, fm_t):
         fm_s = fm_s.view(fm_s.size(0), -1)
-        G_s  = torch.mm(fm_s, fm_s.t())
+        G_s = torch.mm(fm_s, fm_s.t())
         norm_G_s = F.normalize(G_s, p=2, dim=1)
 
         fm_t = fm_t.view(fm_t.size(0), -1)
-        G_t  = torch.mm(fm_t, fm_t.t())
+        G_t = torch.mm(fm_t, fm_t.t())
         norm_G_t = F.normalize(G_t, p=2, dim=1)
 
         loss = F.mse_loss(norm_G_s, norm_G_t)
@@ -618,18 +619,288 @@ class AngularDistance(nn.Module):
             raise ValueError("Invalid reduction mode. Choose between 'mean' and 'sum'.")
 
 
-# class WKD(nn.Module):
-#     def __init__(self, wkd_level=3, wkd_basis='haar'):
-#         super(WKD, self).__init__()
-#         self.xfm = DWTForward(J=wkd_level, wave=wkd_basis, mode='zero')
+class WKD(nn.Module):
+    def __init__(self, wkd_level=3, wkd_basis='haar'):
+        super(WKD, self).__init__()
+        self.xfm = DWTForward(J=wkd_level, wave=wkd_basis, mode='zero')
 
-#     def forward(self, student, teacher):
-#         student_l, student_h = self.xfm(student)
-#         teacher_l, teacher_h = self.xfm(teacher)
-#         loss = 0.0
-#         for index in range(len(student_h)):
-#             loss += torch.nn.functional.l1_loss(teacher_h[index], student_h[index])
-#         return loss
+    def forward(self, student, teacher):
+        student_l, student_h = self.xfm(student)
+        teacher_l, teacher_h = self.xfm(teacher)
+        loss = 0.0
+        for index in range(len(student_h)):
+            loss += torch.nn.functional.l1_loss(teacher_h[index], student_h[index])
+        return loss
+
+
+class DCD(nn.Module):
+    """
+    Discriminative and Consistent Knowledge Distillation
+
+    Args:
+        opt.s_dim: the dimension of student's feature
+        opt.t_dim: the dimension of teacher's feature
+        opt.feat_dim: the dimension of the projection space
+        max_tau (float): Maximum clamp value for the temperature scaling tau.
+        alpha (float): Scaling factor for the KL-divergence part of the loss.
+        init_tau (float): Initial value for the learnable temperature parameter tau.
+        init_b (float): Initial value for the learnable bias parameter b.
+    """
+
+    def __init__(self, s_dim, t_dim, feat_dim=128, max_tau=10.0, alpha=0.5, init_tau=1.0, init_b=0.0):
+        super(DCD, self).__init__()
+        self.params = LearnableParams(init_tau=init_tau, init_b=init_b)
+        self.max_tau = max_tau
+        self.alpha = alpha
+
+        self.embed_s = nn.Linear(s_dim, feat_dim)
+        self.embed_t = nn.Linear(t_dim, feat_dim)
+
+    def forward(self, f_s, f_t):
+        b, c, h, w = f_s.shape
+        b1, c1, h1, w1 = f_t.shape
+        f_s = f_s.view(b, c, -1).mean(2)
+        f_t = f_t.view(b1, c1, -1).mean(2)
+
+        f_s = self.embed_s(f_s)
+        f_t = self.embed_t(f_t)
+
+        f_s = F.normalize(f_s, dim=1)
+        f_t = F.normalize(f_t, dim=1)
+
+        n = f_s.size(0)
+
+        tau = self.params.tau.exp().clamp(0, self.max_tau)
+        b = self.params.b
+
+        logits = torch.mm(f_s, f_t.t()) * tau + b
+        labels = torch.arange(n).to(logits.device)
+
+        # contrastive loss
+        contrastive_loss = F.cross_entropy(logits, labels)
+        p1 = F.log_softmax(logits, dim=1)
+        p2 = F.softmax(logits, dim=0).t()
+
+        # # KL-divergence loss for distribution invariance
+        invariance_loss = F.kl_div(p1, p2, reduction="batchmean")
+
+        total_loss = contrastive_loss + self.alpha * invariance_loss
+        return total_loss
+
+
+class LearnableParams(nn.Module):
+    def __init__(self, init_tau=1.0, init_b=0.0):
+        super(LearnableParams, self).__init__()
+        self.tau = nn.Parameter(torch.tensor(init_tau))  # Learnable temperature parameter
+        self.b = nn.Parameter(torch.tensor(init_b))  # Learnable bias parameter
+
+    def forward(self):
+        # This module doesn't need to do anything in forward, since it's just holding parameters.
+        pass
+
+
+class RRD(nn.Module):
+    """
+    Relational Representation Distillation
+
+    Args:
+        opt.s_dim: the dimension of student's feature
+        opt.t_dim: the dimension of teacher's feature
+        opt.feat_dim: the dimension of the projection space
+        opt.nce_k: number of instances in queue
+        opt.nce_t_s: the temperature (default: 0.02)
+        opt.nce_t_t: the temperature (default: 0.1)
+    """
+
+    def __init__(self, s_dim, t_dim, feat_dim=128, nce_k=4096, nce_t_s=0.1, nce_t_t=0.02):
+        super(RRD, self).__init__()
+        self.nce_k = nce_k      # batch_size * 512
+        self.nce_t_s = nce_t_s
+        self.nce_t_t = nce_t_t
+
+        self.embed_s = nn.Linear(s_dim, feat_dim)
+        self.embed_t = nn.Linear(t_dim, feat_dim)
+
+        self.register_buffer("queue", torch.randn(nce_k, feat_dim))
+        self.queue = nn.functional.normalize(self.queue, dim=0)
+        self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
+
+    def forward(self, f_s, f_t):
+        """
+        Compute the RRD loss between student features (f_s) and teacher features (f_t).
+        """
+        # forward
+        b, c, h, w = f_s.shape
+        b1, c1, h1, w1 = f_t.shape
+        f_s = f_s.view(b, c, -1).mean(2)
+        f_t = f_t.view(b1, c1, -1).mean(2)
+
+        f_s = self.embed_s(f_s)
+        f_t = self.embed_t(f_t)
+
+        # normalize
+        f_s = nn.functional.normalize(f_s, dim=1)
+        f_t = nn.functional.normalize(f_t, dim=1)
+
+        # enqueue and dequeue
+        with torch.no_grad():
+            self._dequeue_and_enqueue(f_t)
+
+        # calculate similarities
+        out_s = torch.einsum("nc,kc->nk", [f_s, self.queue.clone().detach()])
+        out_t = torch.einsum("nc,kc->nk", [f_t, self.queue.clone().detach()])
+
+        # relational loss
+        loss = - torch.sum(nn.functional.softmax(out_t.detach() / self.nce_t_t, dim=1) *
+                           nn.functional.log_softmax(out_s / self.nce_t_s, dim=1), dim=1).mean()
+
+        return loss
+
+    @torch.no_grad()
+    def _dequeue_and_enqueue(self, keys):
+        """
+        Dequeue the oldest batch of features and enqueue the current batch's keys.
+        """
+        batch_size = keys.shape[0]
+        ptr = int(self.queue_ptr)
+        assert self.nce_k % batch_size == 0  # for simplicity
+        self.queue[ptr: ptr + batch_size] = keys
+        ptr = (ptr + batch_size) % self.nce_k  # move pointer
+        self.queue_ptr[0] = ptr
+
+
+class CRD(nn.Module):
+    '''
+    Contrastive Representation Distillation
+    https://openreview.net/pdf?id=SkgpBJrtvS
+
+    includes two symmetric parts:
+    (a) using teacher as anchor, choose positive and negatives over the student side
+    (b) using student as anchor, choose positive and negatives over the teacher side
+
+    Args:
+    s_dim: the dimension of student's feature
+    t_dim: the dimension of teacher's feature
+    feat_dim: the dimension of the projection space
+    nce_n: number of negatives paired with each positive
+    nce_t: the temperature
+    nce_mom: the momentum for updating the memory buffer
+    n_data: the number of samples in the training set, which is the M in Eq.(19)
+    '''
+    def __init__(self, s_dim, t_dim, n_data, feat_dim=128, nce_n=4096, nce_t=0.07, nce_mom=0.5):
+        super(CRD, self).__init__()
+        self.embed_s = Embed(s_dim, feat_dim)
+        self.embed_t = Embed(t_dim, feat_dim)
+        self.contrast = ContrastMemory(feat_dim, n_data, nce_n, nce_t, nce_mom)
+        self.criterion_s = ContrastLoss(n_data)
+        self.criterion_t = ContrastLoss(n_data)
+
+    def forward(self, feat_s, feat_t, idx, sample_idx):
+        feat_s = self.embed_s(feat_s)
+        feat_t = self.embed_t(feat_t)
+        out_s, out_t = self.contrast(feat_s, feat_t, idx, sample_idx)
+        loss_s = self.criterion_s(out_s)
+        loss_t = self.criterion_t(out_t)
+        loss = loss_s + loss_t
+
+        return loss
+
+
+class Embed(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(Embed, self).__init__()
+        self.linear = nn.Linear(in_dim, out_dim)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x = x.view(b, c, -1).mean(2)
+        x = self.linear(x)
+        x = F.normalize(x, p=2, dim=1)
+
+        return x
+
+
+class ContrastLoss(nn.Module):
+    '''
+    contrastive loss, corresponding to Eq.(18)
+    '''
+    def __init__(self, n_data, eps=1e-7):
+        super(ContrastLoss, self).__init__()
+        self.n_data = n_data
+        self.eps = eps
+
+    def forward(self, x):
+        bs = x.size(0)
+        N = x.size(1) - 1
+        M = float(self.n_data)
+
+        # loss for positive pair
+        pos_pair = x.select(1, 0)
+        log_pos = torch.div(pos_pair, pos_pair.add(N / M + self.eps)).log_()
+
+        # loss for negative pair
+        neg_pair = x.narrow(1, 1, N)
+        log_neg = torch.div(neg_pair.clone().fill_(N / M), neg_pair.add(N / M + self.eps)).log_()
+
+        loss = -(log_pos.sum() + log_neg.sum()) / bs
+
+        return loss
+
+
+class ContrastMemory(nn.Module):
+    def __init__(self, feat_dim, n_data, nce_n, nce_t, nce_mom):
+        super(ContrastMemory, self).__init__()
+        self.N = nce_n
+        self.T = nce_t
+        self.momentum = nce_mom
+        self.Z_t = None
+        self.Z_s = None
+
+        stdv = 1. / math.sqrt(feat_dim / 3.)
+        self.register_buffer('memory_t', torch.rand(n_data, feat_dim).mul_(2 * stdv).add_(-stdv))
+        self.register_buffer('memory_s', torch.rand(n_data, feat_dim).mul_(2 * stdv).add_(-stdv))
+
+    def forward(self, feat_s, feat_t, idx, sample_idx):
+        bs = feat_s.size(0)
+        feat_dim = self.memory_s.size(1)
+        n_data = self.memory_s.size(0)
+
+        # using teacher as anchor
+        weight_s = torch.index_select(self.memory_s, 0, sample_idx.view(-1)).detach()
+        weight_s = weight_s.view(bs, self.N + 1, feat_dim)
+        out_t = torch.bmm(weight_s, feat_t.view(bs, feat_dim, 1))
+        out_t = torch.exp(torch.div(out_t, self.T)).squeeze().contiguous()
+
+        # using student as anchor
+        weight_t = torch.index_select(self.memory_t, 0, sample_idx.view(-1)).detach()
+        weight_t = weight_t.view(bs, self.N + 1, feat_dim)
+        out_s = torch.bmm(weight_t, feat_s.view(bs, feat_dim, 1))
+        out_s = torch.exp(torch.div(out_s, self.T)).squeeze().contiguous()
+
+        # set Z if haven't been set yet
+        if self.Z_t is None:
+            self.Z_t = (out_t.mean() * n_data).detach().item()
+        if self.Z_s is None:
+            self.Z_s = (out_s.mean() * n_data).detach().item()
+
+        out_t = torch.div(out_t, self.Z_t)
+        out_s = torch.div(out_s, self.Z_s)
+
+        # update memory
+        with torch.no_grad():
+            pos_mem_t = torch.index_select(self.memory_t, 0, idx.view(-1))
+            pos_mem_t.mul_(self.momentum)
+            pos_mem_t.add_(torch.mul(feat_t, 1 - self.momentum))
+            pos_mem_t = F.normalize(pos_mem_t, p=2, dim=1)
+            self.memory_t.index_copy_(0, idx, pos_mem_t)
+
+            pos_mem_s = torch.index_select(self.memory_s, 0, idx.view(-1))
+            pos_mem_s.mul_(self.momentum)
+            pos_mem_s.add_(torch.mul(feat_s, 1 - self.momentum))
+            pos_mem_s = F.normalize(pos_mem_s, p=2, dim=1)
+            self.memory_s.index_copy_(0, idx, pos_mem_s)
+
+        return out_s, out_t
 
 
 class MLP_Module(nn.Module):
