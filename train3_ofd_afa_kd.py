31ba26ab8ff1abedd7a59e90f9af1161690efa0b
junseo0421 <jmdmk9st@naver.com>
2025-03-17 03:40:52 +0900

    asdf

--- train3_ofd_afa_kd.py
+++ train3_ofd_afa_kd.py
@@ -359,7 +359,7 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer, teacher_gen):
 
 if __name__ == '__main__':
     # model forward 수정해야함!!!
-    NAME_DATASET = 'SDdb-1'
+    NAME_DATASET = 'SDdb-2'
     SAVE_BASE_DIR = '/content/drive/MyDrive/ofd_afa_kd_3/output'
 
     SAVE_WEIGHT_DIR = join(SAVE_BASE_DIR, NAME_DATASET, 'checkpoints')
