c08340f76cbee7ce86c20fbb7f209a5310d9d7a8
junseo0421 <jmdmk9st@naver.com>
2025-03-31 12:03:10 +0900

    25.03.31 vid kd hkdb1

--- loss.py
+++ loss.py
@@ -570,6 +570,43 @@ class VID(nn.Module):
         loss = torch.mean(neg_log_prob)
 
         return loss
+    
+
+class VID_NO_REG(nn.Module):
+    def __init__(self, out_channels, init_var=5.0, eps=1e-6):
+        super(VID_NO_REG, self).__init__()
+        self.eps = eps
+        # self.regressor = nn.Sequential(*[
+        #     nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, bias=False),
+        #     # nn.BatchNorm2d(mid_channels),
+        #     nn.ReLU(),
+        #     nn.Conv2d(mid_channels, mid_channels, kernel_size=1, stride=1, padding=0, bias=False),
+        #     # nn.BatchNorm2d(mid_channels),
+        #     nn.ReLU(),
+        #     nn.Conv2d(mid_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+        # ])
+
+        self.alpha = nn.Parameter(
+            np.log(np.exp(init_var-eps)-1.0) * torch.ones(out_channels)
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            # elif isinstance(m, nn.BatchNorm2d):
+                # 	nn.init.constant_(m.weight, 1)
+                # 	nn.init.constant_(m.bias, 0)
+
+    def forward(self, fm_s, fm_t):
+        # pred_mean = self.regressor(fm_s)
+        pred_var = torch.log(1.0 + torch.exp(self.alpha)) + self.eps
+        pred_var = pred_var.view(1, -1, 1, 1)
+        neg_log_prob = 0.5 * (torch.log(fm_s) + (fm_s-fm_t)**2 / pred_var)
+        loss = torch.mean(neg_log_prob)
+
+        return loss
 
 
 class CosineSimilarity(nn.Module):
