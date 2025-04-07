86265286e7d0754d88c1ba94c9b182d224e8e301
junseo0421 <jmdmk9st@naver.com>
2025-04-08 01:25:33 +0900

    sddb1 threshold 04

--- ablation.py
+++ ablation.py
@@ -420,8 +420,8 @@ def valid(gen, dis, opt_gen, opt_dis, epoch, valid_loader, writer, teacher_gen, 
 
 if __name__ == '__main__':
     # ablation은 SDdb 만 !!!!!!!
-    NAME_DATASET = 'SDdb-2'
-    SAVE_BASE_DIR = '/content/drive/MyDrive/ab2_afa_threshold_02/output'
+    NAME_DATASET = 'SDdb-1'
+    SAVE_BASE_DIR = '/content/drive/MyDrive/ab2_afa_threshold_04/output'
 
     is_afa_hp = False
     is_afa_lp = False
@@ -432,7 +432,7 @@ if __name__ == '__main__':
     is_input_change = True
     is_sobel = True
 
-    afa_threshold = 0.2     # 기본값 0.1
+    afa_threshold = 0.4     # 기본값 0.1
 
     if is_afa_hp or is_afa_lp or is_afa_both:
         is_afa_loss = True
