93fa4ad5fc7b00513808674a0ca3fb94c3868f8f
junseo0421 <jmdmk9st@naver.com>
2025-06-05 02:49:28 +0900

    batch norm 1e-3

--- loss.py
+++ loss.py
@@ -551,7 +551,7 @@ class Proj_1x1(nn.Module):
 
         self.projector = nn.Sequential(*[
             nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1),
-            nn.BatchNorm2d(self.out_channels),
+            nn.BatchNorm2d(self.out_channels, eps=1e-3),
             nn.ReLU(inplace=True)
         ])
 
