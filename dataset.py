ed1761e31702e82ba0c5df94ba578143de4fcdd5
junseo0421 <jmdmk9st@naver.com>
2025-07-30 18:30:05 +0900

    mmcbnu dataset 추가. 나중에 gen, dir 바꿔야함

--- dataset.py
+++ dataset.py
@@ -120,6 +120,61 @@ class dataset_norm(Dataset):
         return self.size
 
 
+class dataset_norm_mmcbnu(Dataset):
+    def __init__(self, root='', transforms=None, imgSize=192, inputsize=92, imglist1=[], imglist2=[], imglist3=[]):  # 24.09.20 수정
+        # --PARAMS--
+        # root: the path of the data
+        # crop: 'rand' or 'center' or 'none', which way to crop the image into target size
+        # imgSize: the size of the returned image if crop is not 'none'
+
+        #self.img_list = []
+        self.transforms = transforms
+        self.imgSize = imgSize
+        self.inputsize = inputsize
+
+        self.img_list1 = imglist1
+        self.img_list2 = imglist2
+        self.img_list3 = imglist3
+
+        self.size = len(self.img_list1)
+
+    def __getitem__(self, index):
+        index = index % self.size
+
+        # 각 경로에서 이미지를 그레이스케일로 로드
+        img1 = Image.open(self.img_list1[index]).convert("L")
+        img2 = Image.open(self.img_list2[index]).convert("L")
+        img3 = Image.open(self.img_list3[index]).convert("L")
+
+        # 이미지를 numpy 배열로 변환
+        img1 = np.array(img1)
+        img2 = np.array(img2)
+        img3 = np.array(img3)
+
+        # 세 이미지 채널을 결합하여 RGB 이미지를 만듭니다.
+        img_cat = np.stack([img1, img2, img3], axis=-1)  # (H, W, C) 형식으로 결합
+
+        # 변환을 적용하기 위해 이미지를 PIL 이미지로 변환
+        img_cat = Image.fromarray(img_cat)
+
+        img_cat = self.transforms(img_cat)
+
+        c0 = img_cat[0].unsqueeze(0)  # (1, H, W)
+        gt = c0.repeat(3, 1, 1)  # (3, H, W)
+
+        i = (self.imgSize - self.inputsize)//2  # 50
+
+        ## 2023 11 14 홍진성 마스킹 수정 (너비 방향으로만 처리)
+        iner_img = img_cat[:, :, i:i+self.inputsize]
+        mask_img = np.ones((3, self.imgSize, self.imgSize))
+        mask_img[:, :, i:i + self.inputsize] = iner_img
+
+        return gt, mask_img
+
+    def __len__(self):
+        return self.size
+
+
 class dataset_norm_ablation(Dataset):
     # prepare data for self-reconstruction,
     # where the two input photos and the intermediate region are obtained from the same image
